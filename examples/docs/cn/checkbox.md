ae5dc5ea200703b6034f03184dada9e3649c0106
Ken Zheng <pengfeizheng@hotmail.com>
2018-12-26 22:58:51 +0800

    1、增加checkbox

--- examples/docs/cn/checkbox.md
+++ examples/docs/cn/checkbox.md
@@ -1,1 +1,10 @@
 **Checkbox 多选框**
+
+<p>
+    <fly-checkbox label='复选框'>
+        复选框
+    </fly-checkbox>
+    <fly-checkbox :checked='true'>
+        复选框2
+    </fly-checkbox>
+</p>
--- src/components/checkbox/src/main.vue
+++ src/components/checkbox/src/main.vue
@@ -1,8 +1,23 @@
 <template>
-    <div></div>
+    <label class='fly-checkbox'>
+        <span :class='["fly-checkbox__input",{
+          "has-checked":checked
+        }]'></span>
+        <span class='fly-checkbox__label'>
+          <slot></slot>
+        </span>
+        <input class='fly-checkbox__input-native'
+          v-model="checked"
+          :value='label'
+          type="checkbox" />
+    </label>
 </template>
 <script>
 export default{
-  name: 'FlyCheckbox'
+  name: 'FlyCheckbox',
+  props: {
+    label: String,
+    checked: false
+  }
 }
 </script>
