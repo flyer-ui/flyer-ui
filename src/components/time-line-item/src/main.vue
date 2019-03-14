0ba631149ca0a3a4d100e5d001451093823ba576
pfzhengd <pengfeizheng@hotmail.com>
2019-03-14 23:57:28 +0800

    1、完善TimeLine组件功能

--- src/components/time-line-item/src/main.vue
+++ src/components/time-line-item/src/main.vue
@@ -1,13 +1,48 @@
 <template>
     <div class='fly-time-line__item'>
-        <slot name='dot'><div class='fly-time-line__dot'></div></slot>
+        <slot name='dot'><div :style='{"background":color}' :class='[
+        "fly-time-line__dot",
+        `fly-time-line__dot--${size}`,
+        `is-${type}`]'><i :class='[icon]'></i></div></slot>
         <div class='fly-time-line__body'>
+          <div v-if='placement==="top"' :class='["fly-time-line__timestamp",`is-${placement}`]'>{{timestamp}}</div>
           <slot></slot>
+          <div v-if='placement==="bottom"' :class='["fly-time-line__timestamp",`is-${placement}`]'>{{timestamp}}</div>
         </div>
     </div>
 </template>
 <script>
 export default {
-  name: 'FlyTimeLineItem'
+  name: 'FlyTimeLineItem',
+  props: {
+    timestamp: String,
+    hideTimestamp: {
+      type: Boolean,
+      default: true
+    },
+    type: {
+      type: String,
+      validator (value) {
+        return ['primary', 'success', 'warning', 'danger', 'info'].indexOf(value) > -1
+      },
+      default: 'default'
+    },
+    placement: {
+      type: String,
+      validator (value) {
+        return ['bottom', 'top'].indexOf(value) > -1
+      },
+      default: 'top'
+    },
+    color: String,
+    size: {
+      type: String,
+      validator (value) {
+        return ['normal', 'large'].indexOf(value) > -1
+      },
+      default: 'normal'
+    },
+    icon: String
+  }
 }
 </script>
