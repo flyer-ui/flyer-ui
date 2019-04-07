2e7afb5c6c2ee26f706f186ab0e37785aa20a2a0
Ken Zheng <pengfeizheng@hotmail.com>
2019-04-07 20:52:52 +0800

    1、整理打包的顺序。

--- src/components/themes/gulpfile.js
+++ src/components/themes/gulpfile.js
@@ -1,9 +1,12 @@
 'use strict'
 
-var gulp = require('gulp')
-var sass = require('gulp-sass')
-var autoprefixer = require('gulp-autoprefixer')
-var cssmin = require('gulp-cssmin')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const path = require('path')
+const autoprefixer = require('gulp-autoprefixer')
+const cssmin = require('gulp-cssmin')
+const libPath = path.resolve(process.cwd(), '.././../../lib')
+console.log(libPath)
 
 gulp.task('compile', function () {
   return gulp.src('./src/*.scss')
@@ -13,13 +16,13 @@ gulp.task('compile', function () {
       cascade: false
     }))
     .pipe(cssmin())
-    .pipe(gulp.dest('./lib'))
+    .pipe(gulp.dest(path.resolve(libPath, 'themes')))
 })
 
 gulp.task('copyfont', function () {
   return gulp.src('./src/fonts/**')
     .pipe(cssmin())
-    .pipe(gulp.dest('./lib/fonts'))
+    .pipe(gulp.dest(path.resolve(libPath, 'themes/fonts')))
 })
 
 gulp.task('build', ['compile', 'copyfont'])
