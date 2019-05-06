994e607810fdb2b880e9fda2a899b365eb06b5a6
Ken Zheng <pengfeizheng@hotmail.com>
2019-05-07 00:32:22 +0800

    20190507

---
+++ examples/docs/cn/pagination.md
@@ -0,0 +1,21 @@
+<script>
+module.exports = {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+## Pagination 分页
+
+### 基础用法
+
+::: demo
+```html
+<template>
+  <fly-pagination :page-size='10' :page-count='5' :total="188"></fly-pagination>
+</template>
+```
+:::
--- src/components/pagination/src/main.vue
+++ src/components/pagination/src/main.vue
@@ -1,8 +1,162 @@
-<template>
-    <div class='fly-pagination'></div>
-</template>
 <script>
 export default {
-  name: 'FlyPagination'
+  name: 'FlyPagination',
+  props: {
+    pageSize: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    pageStart: {
+      type: Number,
+      default: 1
+    },
+    pageCount: {
+      type: Number,
+      default: 7
+    },
+    total: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    value: {
+      type: Number,
+      default: 1
+    },
+    prevText: String,
+    nextText: String
+  },
+  data () {
+    return {
+      index: 0,
+      start: 0
+    }
+  },
+  computed: {
+    max () {
+      return Math.ceil(this.total / this.pageSize)
+    },
+    mini () {
+      return this.pageStart
+    },
+    balance () {
+      // 减去最大值与最小值
+      return this.pageCount - 2
+    },
+    increment () {
+      return Math.ceil(this.balance / 2)
+    },
+    balanceStart: {
+      get () {
+        if (this.start === this.pageStart) {
+          return this.pageStart
+        }
+        return this.start - this.increment
+      },
+      set (value) {
+        this.start = value
+      }
+    },
+    currentPage: {
+      get () {
+        return this.index
+      },
+      set (value) {
+        this.index = value
+        if (value >= this.balanceEnd || value <= this.balanceStart) {
+          this.balanceStart = value
+        }
+      }
+    },
+    balanceEnd () {
+      let end = 0
+      if (this.currentPage === this.pageStart) {
+        end = this.currentPage + this.balance
+      } else {
+        end = this.currentPage + this.increment
+      }
+      return end <= this.max ? end : this.max
+    },
+    showMiniLimit () {
+      return this.currentPage >= this.mini + this.balance
+    },
+    showMaxLimit () {
+      return this.currentPage <= this.max - this.balance
+    },
+    isFirstPage () {
+      return this.currentPage === this.mini
+    },
+    isLastPage () {
+      return this.currentPage === this.max
+    }
+  },
+  methods: {
+    handleClick (number) {
+      this.currentPage = number
+      this.$emit('input', this.currentPage)
+      this.$emit('on-change', this.currentPage)
+    },
+    handlePrev () {
+      if (this.isFirstPage) {
+        return false
+      }
+      this.currentPage--
+      this.$emit('input', this.currentPage)
+      this.$emit('on-change', this.currentPage)
+    },
+    handleNext () {
+      if (this.isLastPage) {
+        return false
+      }
+      this.currentPage++
+      this.$emit('input', this.currentPage)
+      this.$emit('on-change', this.currentPage)
+    },
+    showLimt (has) {
+      if (!has) { return '' }
+      return (<li class={['fly-pagination__number']}>
+          ...
+      </li>)
+    }
+  },
+  created () {
+    this.currentPage = this.value
+    this.balanceStart = this.value
+  },
+  render (h) {
+    return (
+      <div class='fly-pagination'>
+        <ul class='fly-pagination__pager'>
+          <li class={['fly-pagination__number', {'is-disabled': this.isFirstPage}]} on-click={this.handlePrev}>
+            <i class='fly-icon-chevron-left'></i>
+          </li>
+          <li class={['fly-pagination__number',
+            {'is-active': this.mini === this.currentPage}]} on-click={($event) => { this.handleClick(this.mini) }}>
+            {this.mini}
+          </li>
+          {this.showLimt(this.showMiniLimit)}
+          {this._l(this.balanceEnd, (number, index) => {
+            console.log('balanceEnd', this.balanceEnd)
+            console.log('balanceStart', this.balanceStart)
+            console.log('currentPage', this.currentPage)
+            console.log('number', number)
+            console.log('increment', this.increment)
+            if (number < this.balanceStart || number === this.mini || number === this.max) { return '' }
+            return (<li class={['fly-pagination__number',
+              {'is-active': number === this.currentPage}]} on-click={($event) => { this.handleClick(number) }}>
+              {number}
+            </li>)
+          })}
+          {this.showLimt(this.showMaxLimit)}
+          <li class={['fly-pagination__number', {'is-active': this.max === this.currentPage}]} on-click={$event => { this.handleClick(this.max) }}>
+            {this.max}
+          </li>
+          <li class={['fly-pagination__number', {'is-disabled': this.isLastPage}]} on-click={this.handleNext}>
+            <i class='fly-icon-chevron-right'></i>
+          </li>
+        </ul>
+      </div>)
+  }
 }
 </script>
