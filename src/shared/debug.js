ae36a3eae9604e0494a383f14443958c0daa528c
pfzhengd <pengfeizheng@hotmail.com>
2019-10-22 23:18:08 +0800

    添加debug

---
+++ src/shared/debug.js
@@ -0,0 +1,11 @@
+export const warn = function (msg) {
+  console.warn(msg)
+}
+
+export const error = function (msg) {
+  console.error(msg)
+}
+
+export const tip = function (msg) {
+  console.log(msg)
+}
