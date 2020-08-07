91616aea805b0c2df6871acbd3f28e7865a47a15
pfzheng <pengfeizheng@hotmail.com>
2020-08-07 17:23:17 +0800

    revert: 回滚select组件。

---
+++ src/components/select/src/option.vue
@@ -0,0 +1,44 @@
+<template>
+    <li :class='[
+    "fly-select__option",
+    {"is-disabled":disabled},
+    {"is-selected":hasSeleted}
+    ]' @mouseup.stop='handleClick'>
+        <slot name='default'>{{label}}</slot>
+    </li>
+</template>
+<script>
+import {findParentByName} from '~/utils'
+export default {
+  name: 'FlyOption',
+  inject: ['select'],
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    label: String,
+    value: {
+      required: false
+    }
+  },
+  computed: {
+    parent () {
+      return findParentByName('FlySelect', this)
+    },
+    hasSeleted () {
+      return Array.isArray(this.select.value) ? this.select.value.findIndex((value) => {
+        return value === this.value
+      }) > -1 : this.select.value === this.value
+    }
+  },
+  methods: {
+    handleClick ($event) {
+      if (this.disabled) {
+        return
+      }
+      this.parent.executeSelected({label: this.label, value: this.value}, $event)
+    }
+  }
+}
+</script>
