63a4dbce2592624a9ec49777bf086bc2f33ba408
pfzheng <pengfeizheng@hotmail.com>
2020-09-21 17:48:43 +0800

    feat: 添加表格过滤面板代码。

---
+++ src/components/table/src/filter-panel.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class='fly-table__filter-panel'>
+        FlyTableFilterPanel
+    </div>
+</template>
+
+<script>
+export default {
+    name:'FlyTableFilterPanel'
+}
+</script>
